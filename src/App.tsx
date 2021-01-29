6f11a8201717034df885baab7f3036d13e5cb792
nendosoft <nendosoft@gmail.com>
2021-01-29 18:03:36 +0900

    react router 6

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,30 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Navigate, Route, Routes, useLocation } from "react-router-dom"; // npm install react-router@next react-router-dom@next history
 
 import ProductPage from "./components/pages/ProductPage";
 import HomePage from "components/pages/HomePage";
-import Demo from "containers/templates/Demo";
+// import Demo from "containers/templates/Demo";
 import NotFound from "components/pages/NotFound";
-import orgCodes from "data/org-codes";
-
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <Switch>
-        {/* <Route path="/test" component={TreeView01} exact /> */}
-        <Route path="/" component={HomePage} exact />
-        <Route path="/:orgCode/members" component={Demo} />
-        <Route path="/products" component={ProductPage} exact />
-        {/* <Route path="/test" component={Demo} exact /> */}
-        {/* <Route path="/:orgCode/members" element={<Members />} /> */}
-        {/* <Route component={NotFound} /> */}
-        <Route component={HomePage} />
-      </Switch>
-    </Router>
+    // <Router>
+    //   <Switch>
+    //     <Route path="/" component={HomePage} exact />
+    //     {/* <Route path="/:orgCode/members" component={Demo} /> */}
+    //     <Route path="/products" component={ProductPage} exact />
+    //     <Route component={NotFound} />
+    //     {/* <Route component={HomePage} /> */}
+    //   </Switch>
+    // </Router>
+
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/products" element={<ProductPage />} />
+      {/* <Route path="*" element={<Navigate to="/" replace />} />; */}
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   );
 };
 
